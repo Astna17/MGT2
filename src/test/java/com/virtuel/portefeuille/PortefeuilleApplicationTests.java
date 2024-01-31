f362edd245ff06ce644e344a246a9a878d4ccc52
Astna17 <hei.antsa.17@gmail.com>
2024-01-31 14:26:24 +0300

    first commit

---
+++ src/test/java/com/virtuel/portefeuille/PortefeuilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtuel.portefeuille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortefeuilleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
